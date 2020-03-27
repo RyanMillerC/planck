101068fff30d1e2216ab940425a7eb20b7d36b3d
Ryan Miller <ryan@devopsmachine.com>
2020-03-26 23:21:32 -0400

    Swap place of Ctrl/GUI on both sides

--- keymap.c
+++ keymap.c
@@ -50,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+------+------+------+------+------+------|
    * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  | Shift|
    * |------+------+------+------+------+------+------+------+------+------+------+------|
-   * | Func | GUI  | Ctrl | Alt  | Lower|    Space    | Raise| Alt  | Ctrl | GUI  |CapsLk|
+   * | Func | Ctrl | GUI  | Alt  | Lower|    Space    | Raise| Alt  | GUI  | Ctrl |CapsLk|
    * `-----------------------------------------------------------------------------------'
    *  Tab*   --> Hold for Navigation layer; Press for Tab
    *  Quote* --> Hold for Shift; Press for quote
@@ -60,7 +60,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_ESC,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
       NV_TAB,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    NV_SEMI, KC_ENT,
       KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-      FUNCTION, KC_LGUI, KC_LCTL, KC_LALT, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_RALT, KC_RCTL, KC_RGUI, KC_CAPS
+      FUNCTION, KC_LCTL, KC_LGUI, KC_LALT, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_RALT, KC_RGUI, KC_RCTL, KC_CAPS
   ),
 
   /* Lower
